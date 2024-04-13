18398eb4d83a29471468be2d92d5530b7e510d1b
salverius-tech <salverius-tech@users.noreply.github.com>
2024-04-13 09:28:53 -0700

    Cleanup of backup.mk

--- make.d/backup.mk
+++ make.d/backup.mk
@@ -4,11 +4,14 @@
 ##
 #########################################################
 
+$(NFS_BACKUP_TMP_DIR):
+	sudo mkdir -p $(NFS_BACKUP_TMP_DIR)
+	sudo mount -t nfs $(NFS_SERVER):$(NFS_BACKUP_PATH) $(NFS_BACKUP_TMP_DIR)
+	
 create-backup: backups ## create a backup of the onramp config
 	sudo tar --exclude=.keep --exclude=etc/plex/Library -czf ./backups/onramp-config-backup-$(HOST_NAME)-$(shell date +'%y-%m-%d-%H%M').tar.gz ./etc ./services-enabled ./overrides-enabled .env || true
 
-create-nfs-backup: create-backup ## create a backup of the onramp config and copy it to the nfs server
-	sudo mount -t nfs $(NFS_SERVER):$(NFS_BACKUP_PATH) $(NFS_BACKUP_TMP_DIR)
+create-nfs-backup: $(NFS_BACKUP_TMP_DIR) create-backup ## create a backup of the onramp config and copy it to the nfs server
 	sudo mv ./backups/onramp-config-backup* $(NFS_BACKUP_TMP_DIR)
 	sudo umount $(NFS_BACKUP_TMP_DIR)
 
@@ -18,10 +21,6 @@ backups: ## create backups folder
 restore-backup: ## restore the latest backup of the onramp config
 	sudo tar -xvf ./backups/onramp-config-backup-$(HOST_NAME)-*.tar.gz
 
-$(NFS_BACKUP_TMP_DIR):
-	sudo mkdir -p $(NFS_BACKUP_TMP_DIR)
-	sudo mount -t nfs $(NFS_SERVER):$(NFS_BACKUP_PATH) $(NFS_BACKUP_TMP_DIR)
-	
 restore-nfs-backup: $(NFS_BACKUP_TMP_DIR) backups ## restore the latest backup of the onramp config from the nfs server
 	$(eval BACKUP_FILE := $(shell find $(NFS_BACKUP_TMP_DIR)/*$(HOST_NAME)* -type f -printf "%T@ %p\n" | sort -n | cut -d' ' -f 2- | tail -n 1))
 	sudo rm -rf ./backups/*
