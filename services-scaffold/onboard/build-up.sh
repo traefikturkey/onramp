644d3c3be127d13ed649a4c4c59842812ec41ee5
salverius-tech <salverius-tech@users.noreply.github.com>
2024-04-17 17:27:45 -0700

    added scaffold for onboard

---
+++ services-scaffold/onboard/build-up.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+###############################################
+# ONBOARD build up script
+###############################################
+
+# Include in this script any commands related to the build up of this service.
+
+mkdir -p ./etc/onboard/cache
