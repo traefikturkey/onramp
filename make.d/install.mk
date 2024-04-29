357a30e02270aa2a646e61c310ae3fb3a379d3bb
mike <mglenn@ilude.com>
2024-04-29 19:25:12 -0400

    restructure makefile includes so that we do not have to manually add new files to the Makefile

--- make.d/install.mk
+++ make.d/install.mk
@@ -83,3 +83,33 @@ install-nvidia-drivers: install-ansible
 # kill all vscode instances running on the server
 make kill-code:
 	ps aux | grep .vscode-server | awk '{print $$2}' | xargs kill
+
+#########################################################
+##
+## staging commands
+##
+#########################################################
+
+start-staging: build ## start the staging and wait for the acme staging certs to be issued
+	ACME_CASERVER=https://acme-staging-v02.api.letsencrypt.org/directory $(DOCKER_COMPOSE) $(DOCKER_COMPOSE_FLAGS) up -d --force-recreate
+	@echo "waiting $(CF_RESOLVER_WAITTIME) seconds for cert DNS propogation..."
+	@echo ""
+	@echo "While you wait you should setup a DNS record pointing $(HOST_NAME).$(HOST_DOMAIN) to $(HOSTIP) for this server!"
+	@echo "If you will be using Joyride here are the entries you will need for dnsmasq/pihole:"
+	@echo ""
+	@echo "address=/$(HOST_NAME).$(HOST_DOMAIN)/$(HOSTIP)"
+	@echo "server=/$(HOST_DOMAIN)/$(HOSTIP)#54"
+	@echo ""
+	@sleep $(CF_RESOLVER_WAITTIME)
+	@echo "open https://$(HOST_NAME).$(HOST_DOMAIN)/traefik in a browser"
+	@echo "and check that you have a staging cert from LetsEncrypt!"
+	@echo ""
+	@echo "if you don't get a LetsEncrypt staging cert run the following command and look for error messages:"
+	@echo "docker compose logs | grep acme"
+	@echo ""
+	@echo "otherwise run the following command if you successfully got a staging certificate:"
+	@echo "make down-staging"
+
+down-staging: ## stop the staging and delete the acme staging certs
+	$(DOCKER_COMPOSE) $(DOCKER_COMPOSE_FLAGS) down
+	$(MAKE) clean-acme
---
+++ make.d/utils.mk
@@ -0,0 +1,46 @@
+#########################################################
+##
+## helper commands
+##
+#########################################################
+
+bash-run:
+	$(DOCKER_COMPOSE) $(DOCKER_COMPOSE_FLAGS) run -it --rm $(SERVICE_PASSED_DNCASED) sh
+
+bash-exec:
+	$(DOCKER_COMPOSE) $(DOCKER_COMPOSE_FLAGS) exec $(SERVICE_PASSED_DNCASED) sh
+
+edit-env: ## edit the .env file using the editor specified in the EDITOR variable
+	$(EDITOR) .env
+
+generate-matrix-config:
+	docker run -it --rm  -v ./etc/synapse:/data  -e SYNAPSE_SERVER_NAME=synapse.traefikturkey.icu -e SYNAPSE_REPORT_STATS=yes matrixdotorg/synapse:latest generate	
+
+#########################################################
+##
+## arrs api-key retrieval
+##
+#########################################################
+
+retrieve-apikey: ## retrieve api key from arrs
+	@grep -oP '(?<=ApiKey>).*?(?=</ApiKey>)' ./etc/$${SERVICE_PASSED_DNCASED}/config.xml
+
+
+#########################################################
+##
+## test and debugging commands
+##
+#########################################################
+
+excuse: ## get programming excuse
+	@curl -s programmingexcuses.com | egrep -o "<a[^<>]+>[^<>]+</a>" | egrep -o "[^<>]+" | sed -n 2p
+
+test-smtp: ## test smtp
+	envsubst .templates/smtp.template | nc localhost 25
+
+# https://stackoverflow.com/questions/7117978/gnu-make-list-the-values-of-all-variables-or-macros-in-a-particular-run
+echo:
+	@$(MAKE) -pn | grep -A1 "^# makefile"| grep -v "^#\|^--" | grep -e "^[A-Z]+*" | sort
+
+env: ## show environment variables sorted
+	@env | sort
