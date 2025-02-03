61590301f92ebc826769c1fe1ee5ba105a98d859
salverius-tech <salverius-tech@users.noreply.github.com>
2025-02-02 16:53:51 -0800

    Added service scaffold for homarr

---
+++ services-scaffold/homarr/build-up.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+###############################################
+# HOMARR build up script
+###############################################
+
+# Include in this script any commands related to the build up of this service.
+
+mkdir -p ./etc/homarr/configs
+mkdir -p ./etc/homarr/icons
+mkdir -p ./etc/homarr/data
+mkdir -p ./etc/homarr/imgs/backgrounds
