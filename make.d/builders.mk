24281c262a4862f8e7700f7f4c55499110845f03
mike <219478+ilude@users.noreply.github.com>
2025-04-01 13:20:18 -0700

    tweaks and cleanup

--- make.d/builders.mk
+++ make.d/builders.mk
@@ -9,54 +9,54 @@ BUILD_DEPENDENCIES += $(filter-out $(BUILD_DEPENDENCIES),etc/authelia/configurat
 endif
 
 etc/authelia/configuration.yml:
-	mkdir -p ./etc/authelia
-	envsubst '$${HOST_DOMAIN}' < .templates/authelia_configuration.template > ./etc/authelia/configuration.yml
+	@mkdir -p ./etc/authelia
+	@envsubst '$${HOST_DOMAIN}' < .templates/authelia_configuration.template > ./etc/authelia/configuration.yml
 
 ifneq (,$(wildcard ./services-enabled/adguard.yml))
 BUILD_DEPENDENCIES += $(filter-out $(BUILD_DEPENDENCIES),etc/adguard/conf/AdGuardHome.yaml)
 endif
 
 etc/adguard/conf/AdGuardHome.yaml:
-	mkdir -p ./etc/adguard/conf
-	envsubst '$${ADGUARD_PASSWORD}, $${ADGUARD_USER}, $${HOST_DOMAIN}' < .templates/adguardhome.template > ./etc/adguard/conf/AdGuardHome.yaml
+	@mkdir -p ./etc/adguard/conf
+	@envsubst '$${ADGUARD_PASSWORD}, $${ADGUARD_USER}, $${HOST_DOMAIN}' < .templates/adguardhome.template > ./etc/adguard/conf/AdGuardHome.yaml
 
 ifneq (,$(wildcard ./services-enabled/searnxg.yml))
 BUILD_DEPENDENCIES += $(filter-out $(BUILD_DEPENDENCIES),etc/searnxg/settings.yaml)
 endif
 
 etc/searnxg/settings.yaml:
-	mkdir -p ./etc/searnxg
-	envsubst < .templates/searxng.settings.template.yml > ./etc/searnxg/settings.yml
+	@mkdir -p ./etc/searnxg
+	@envsubst < .templates/searxng.settings.template.yml > ./etc/searnxg/settings.yml
 
 ifneq (,$(wildcard ./services-enabled/pihole.yml))
 BUILD_DEPENDENCIES += $(filter-out $(BUILD_DEPENDENCIES),etc/pihole/dnsmasq/03-custom-dns-names.conf)
 endif
 
 etc/pihole/dnsmasq/03-custom-dns-names.conf:
-	envsubst '$${HOST_DOMAIN}, $${HOSTIP} ' < .templates/pihole_joyride.template > ./etc/pihole/dnsmasq/03-custom-dns-names.conf
+	@envsubst '$${HOST_DOMAIN}, $${HOSTIP} ' < .templates/pihole_joyride.template > ./etc/pihole/dnsmasq/03-custom-dns-names.conf
 
 ifneq (,$(wildcard ./services-enabled/dashy.yml))
 BUILD_DEPENDENCIES += $(filter-out $(BUILD_DEPENDENCIES),etc/dashy/dashy-config.yml)
 endif
 
 etc/dashy/dashy-config.yml:
-	mkdir -p ./etc/dashy
-	touch ./etc/dashy/dashy-config.yml
+	@mkdir -p ./etc/dashy
+	@touch ./etc/dashy/dashy-config.yml
 
 ifneq (,$(wildcard ./services-enabled/prometheus.yml))
 BUILD_DEPENDENCIES += $(filter-out $(BUILD_DEPENDENCIES),etc/prometheus/conf)
 endif
 
 etc/prometheus/conf:
-	mkdir -p etc/prometheus/conf
-	cp --no-clobber --recursive	.templates/prometheus-conf/* etc/prometheus/conf
+	@mkdir -p etc/prometheus/conf
+	@cp --no-clobber --recursive	.templates/prometheus-conf/* etc/prometheus/conf
 
 ifneq (,$(wildcard ./services-enabled/recyclarr.yml))
 BUILD_DEPENDENCIES += $(filter-out $(BUILD_DEPENDENCIES),etc/recyclarr/recyclarr.yml)
 endif
 
 etc/recyclarr/recyclarr.yml:
-	cp --no-clobber .templates/recyclarr.template .etc/recyclarr/recyclarr.yml
+	@cp --no-clobber .templates/recyclarr.template .etc/recyclarr/recyclarr.yml
 
 
 ifneq (,$(wildcard ./services-enabled/gatus.yml))
@@ -64,14 +64,14 @@ BUILD_DEPENDENCIES += $(filter-out $(BUILD_DEPENDENCIES),etc/gatus/config.yaml)
 endif
 
 etc/gatus/config.yaml:
-	cp --no-clobber .templates/gatus.template .etc/gatus/config.yaml
+	@cp --no-clobber .templates/gatus.template .etc/gatus/config.yaml
 
 ifneq (,$(wildcard ./services-enabled/olivetin.yml))
 BUILD_DEPENDENCIES += $(filter-out $(BUILD_DEPENDENCIES),etc/olivetin/config.yaml)
 endif
 
 etc/olivetin/config.yaml:
-	touch $@
+	@touch $@
 
 ifneq (,$(wildcard ./services-enabled/cloudflare-tunnel.yml))
 BUILD_DEPENDENCIES += $(filter-out $(BUILD_DEPENDENCIES),cloudflare-tunnel)
@@ -125,7 +125,7 @@ endif
 
 setup-wordpress-upload:
 	@mkdir -p ./etc/wordpress
-	cp --no-clobber .templates/wordpress-upload.template .etc/wordpress/upload.ini
+	@cp --no-clobber .templates/wordpress-upload.template .etc/wordpress/upload.ini
 
 
 ifneq (,$(wildcard ./etc/traefik/letsencrypt/acme.json))
@@ -133,6 +133,6 @@ BUILD_DEPENDENCIES += $(filter-out $(BUILD_DEPENDENCIES),fix-acme-json-permissio
 endif
 
 fix-acme-json-permissions:
-	sudo chmod 600 ./etc/traefik/letsencrypt/acme.json
+	@sudo chmod 600 ./etc/traefik/letsencrypt/acme.json
 
 #$(info "builders.mk loaded")
