7364c546504375178098c2ea21cd7f7873e84d26
salverius-tech <salverius-tech@users.noreply.github.com>
2025-09-14 08:42:36 -0700

    Removed branch specific path from backup command

--- make.d/backup.mk
+++ make.d/backup.mk
@@ -41,7 +41,7 @@ create-nfs-backup-direct: $(NFS_BACKUP_TMP_DIR) ## create a backup of the onramp
 		sudo mkdir -p $(NFS_BACKUP_TMP_DIR)
 		sudo mount -t nfs $(NFS_SERVER):$(NFS_BACKUP_PATH) $(NFS_BACKUP_TMP_DIR)
 		fi
-	sudo tar --exclude=.keep $(ONRAMP_BACKUP_EXCLUSIONS:=--exclude=etc/plex/Library) -czf $(NFS_BACKUP_TMP_DIR)/onramp-config-backup-$(HOST_NAME)-$(shell date +'%y-%m-%d-%H%M').tar.gz ./etc ./services-enabled ./overrides-enabled ./environments-enabled $(ONRAMP_BACKUP_INCLUSIONS) || true
+	sudo tar --exclude=.keep $(ONRAMP_BACKUP_EXCLUSIONS:=--exclude=etc/plex/Library) -czf $(NFS_BACKUP_TMP_DIR)/onramp-config-backup-$(HOST_NAME)-$(shell date +'%y-%m-%d-%H%M').tar.gz ./etc ./services-enabled ./overrides-enabled $(ONRAMP_BACKUP_INCLUSIONS) || true
 	sudo umount $(NFS_BACKUP_TMP_DIR)
 	sudo rm -r $(NFS_BACKUP_TMP_DIR)	
 
