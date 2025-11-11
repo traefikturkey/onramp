e6d8c26af194035263c6612f12efe552a964644f
sudo-battlekafer <battlekafer@gmail.com>
2025-11-11 23:59:49 +0000

    adding arr_scripts to sonarr/radarr

--- make.d/builders.mk
+++ make.d/builders.mk
@@ -183,12 +183,55 @@ ifneq (,$(wildcard ./services-enabled/radarr.yml))
 BUILD_DEPENDENCIES += $(filter-out $(BUILD_DEPENDENCIES),build_radarr_dirs)
 endif
 
-build_radarr_dirs:
-	mkdir -p ./etc/radarr/custom-services.d
-	mkdir -p ./etc/radarr/custom-cont-init.d
-	cp --no-clobber --recursive	.templates/radarr_extended.template etc/radarr/extended.conf
-	# wget -P ./etc/radarr/custom-cont-init.d https://raw.githubusercontent.com/RandomNinjaAtk/arr-scripts/main/radarr/scripts_init.bash
+	if [ -z "$$(ls -A ./etc/radarr/custom-services.d 2>/dev/null)" ]; then \
+		echo "custom-services.d is empty → downloading scripts_init.bash"; \
+		wget -q -P ./etc/radarr/custom-cont-init.d \
+			https://raw.githubusercontent.com/RandomNinjaAtk/arr-scripts/main/radarr/scripts_init.bash; \
+	else \
+		echo "custom-services.d has content → skipping download and cleaning old init script"; \
+		if [ -f ./etc/radarr/custom-cont-init.d/scripts_init.bash ]; then \
+			rm -f ./etc/radarr/custom-cont-init.d/scripts_init.bash; \
+			echo "Removed ./etc/radarr/custom-cont-init.d/scripts_init.bash"; \
+		fi; \
+	fi
+
+	if [ ! -f ./etc/radarr/extended.conf ]; then \
+		echo "extended.conf not found → creating from template"; \
+		cp ./templates/radarr_extended.template ./etc/radarr/extended.conf; \
+	else \
+		echo "extended.conf already exists → skipping creation"; \
+	fi
 	chown -R $USER:$USER ./etc/radarr
 	chmod g+s ./etc/radarr
+
+ifneq (,$(wildcard ./services-enabled/sonarr.yml))
+BUILD_DEPENDENCIES += $(filter-out $(BUILD_DEPENDENCIES),build_sonarr_dirs)
+endif
+
+build_sonarr_dirs:
+	@echo "Creating sonarr directories..."
+	mkdir -p ./etc/sonarr/custom-services.d
+	mkdir -p ./etc/sonarr/custom-cont-init.d
+
+	if [ -z "$$(ls -A ./etc/sonarr/custom-services.d 2>/dev/null)" ]; then \
+		echo "custom-services.d is empty → downloading scripts_init.bash"; \
+		wget -q -P ./etc/sonarr/custom-cont-init.d \
+			https://raw.githubusercontent.com/RandomNinjaAtk/arr-scripts/main/sonarr/scripts_init.bash; \
+	else \
+		echo "custom-services.d has content → skipping download and cleaning old init script"; \
+		if [ -f ./etc/sonarr/custom-cont-init.d/scripts_init.bash ]; then \
+			rm -f ./etc/sonarr/custom-cont-init.d/scripts_init.bash; \
+			echo "Removed ./etc/sonarr/custom-cont-init.d/scripts_init.bash"; \
+		fi; \
+	fi
+
+	if [ ! -f ./etc/sonarr/extended.conf ]; then \
+		echo "extended.conf not found → creating from template"; \
+		cp ./templates/sonarr_extended.template ./etc/sonarr/extended.conf; \
+	else \
+		echo "extended.conf already exists → skipping creation"; \
+	fi
+	chown -R $USER:$USER ./etc/sonarr
+	chmod g+s ./etc/sonarr
 
 #$(info "builders.mk loaded")
