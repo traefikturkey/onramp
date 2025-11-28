ed94bd9ddff261f4b6994170a9533e14e7a62485
Mike Glenn <mglenn@ilude.com>
2025-11-27 20:12:00 -0500

    feat(scaffold): migrate copyparty to services-scaffold/
    Migrated copyparty configuration from legacy builders.mk rule to
    convention-based services-scaffold/ system. Static config file
    is now copied during enable-service via scaffold-build.

--- make.d/builders.mk
+++ make.d/builders.mk
@@ -33,14 +33,6 @@ etc/pocketbase:
 	mkdir -p ./etc/pocketbase/public
 	mkdir -p ./etc/pocketbase/hooks
 
-ifneq (,$(wildcard ./services-enabled/copyparty.yml))
-BUILD_DEPENDENCIES += $(filter-out $(BUILD_DEPENDENCIES),etc/copyparty/config/copyparty.conf)
-endif
-
-etc/copyparty/config/copyparty.conf:
-	mkdir -p ./etc/copyparty/config
-	cp --no-clobber .templates/copyparty.conf etc/copyparty/config/copyparty.conf
-
 ifneq (,$(wildcard ./services-enabled/dashy.yml))
 BUILD_DEPENDENCIES += $(filter-out $(BUILD_DEPENDENCIES),etc/dashy/dashy-config.yml)
 endif
