6b6a2c8684f4a7b33f627901297d3bacec91e640
Mike Glenn <mglenn@ilude.com>
2025-11-28 01:27:29 -0500

    chore: clean up Makefile legacy code and comments
    Removed dead code from help.mk and install.mk:
    - Deleted commented-out help target duplicate
    - Removed legacy .env backwards-compat target
    - Removed dead NETBOX_EMAIL conditional
    - Fixed typo in kill-code target
    - Improved comment clarity (addrepo, help function source)

--- make.d/help.mk
+++ make.d/help.mk
@@ -1,19 +1,11 @@
 #########################################################
 ##
-## help commands
+## Help Commands
 ##
 #########################################################
 
-
-# help:  ## show this help text
-# 	@awk -vG=$$(tput setaf 2) -vR=$$(tput sgr0) ' \
-# 	  match($$0, "^(([^#:]*[^ :]) *:)?([^#]*)##([^#].+|)$$",a) { \
-# 	    if (a[2] != "") { printf "    make %s%-18s%s %s\n", G, a[2], R, a[4]; next }\
-# 	    if (a[3] == "") { print a[4]; next }\
-# 	    printf "\n%-36s %s\n","",a[4]\
-# 	  }' Makefile
-
-#Extract the help command logic into a function
+# Parses ## comments after targets to generate help text
+# Source: https://gist.github.com/prwhite/8168133
 define help_function
 	@awk -vG=$$(tput setaf 2) -vR=$$(tput sgr0) ' \
 		match($$0, "^(([^#:]*[^ :]) *:)?([^#]*)##([^#].+|)$$",a) { \
@@ -23,12 +15,5 @@ define help_function
 		}' $(1)
 endef
 
-# # Define the help target and call the help function
-help:
+help: ## Show available targets
 	$(call help_function, Makefile)
-
-
-# .DEFAULT_GOAL := help
-
-# Stolen from here 
-# https://gist.github.com/prwhite/8168133
